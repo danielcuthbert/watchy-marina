683cc8588fa6ac45d6ab7da9d890c9ce98562b13
Daniel Cuthbert <daniel@seventysix.io>
2021-07-24 19:14:50 +0100

    first commit of marina

---
+++ README.md
@@ -0,0 +1,12 @@
+# Marina
+
+
+
+![marina](img/marina.jpg)
+
+My first attempt at a watch face for the https://watchy.sqfmi.com/
+
+Todo: 
+
+- make the battery look better, it really looks like shit
+- balance the display out better
---
+++ src/marina.ino
@@ -0,0 +1,93 @@
+/***************************************************
+ Watch face for a Watchy Open Source E-Paper Watch
+ V0.1
+ Daniel Cuthbert 2021
+ ****************************************************/
+
+#include <Watchy.h> //include the Watchy library
+#include "marina_opentype12pt7b.h"
+#include "marina_opentype50pt7b.h" // 50pt works but maybe smaller will be better
+				
+
+class WatchFace : public Watchy { //inherit and extend Watchy class
+  public:
+    void drawWatchFace() { //where the main look of the watch face goes
+      
+      int16_t  x1, y1; // Display is a 200x200 GDEH0154D67
+      uint16_t w, h;
+      String textstring;
+   
+      // We don't want the activity sensor to roll over, so reset it at 00:00
+      if(currentTime.Hour == 00 && currentTime.Minute == 00) {
+        sensor.resetStepCounter();
+      }
+      
+      // Make the main watch face black 
+      // This is where the hour/day will be shown in 50pt
+      display.fillScreen(GxEPD_BLACK);
+      display.setTextColor(GxEPD_WHITE);
+      display.setTextWrap(false);
+      display.setFont(&marina_opentype_bigfontsite_com50pt7b);
+     
+      //Hour of Day
+      textstring = currentTime.Hour;
+      display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
+      display.setCursor(175-w, 85-5);
+      display.print(textstring);
+      
+      //Minutes
+      if (currentTime.Minute < 10) {
+        textstring = "0";
+      } else {
+        textstring = "";
+      }
+      textstring += currentTime.Minute;
+      display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
+      display.setCursor(185-w, 85+3+h);
+      display.print(textstring);
+
+      // Battery Level
+      // this needs more work, it looks shit 
+      float batt = (getBatteryVoltage()-3.3)/0.9;
+      if (batt > 0) {
+       display.fillRect(20,20,26*batt,4,GxEPD_WHITE);
+      }									
+
+      // Second part of the display
+      // This is where the date/month and step counter is shown
+      // We need a smaller font
+      display.setFont(&marina_opentype_bigfontsite_com12pt7b);
+
+      //Our Step Counter
+      textstring = sensor.getCounter();
+      textstring += " Steps";
+      display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
+      display.setCursor(8, 181);
+      display.print(textstring);
+      
+      // The Day
+      textstring = dayStr(currentTime.Wday);
+      display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
+      display.setCursor(8, 100);
+      display.print(textstring);
+            
+      // The Month
+      textstring = monthShortStr(currentTime.Month);
+      textstring += " ";
+      textstring += currentTime.Day;
+      display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
+      display.setCursor(8, 70);
+      display.print(textstring);
+ 
+    }
+};
+
+WatchFace m; //instantiate your watchface
+
+void setup() {
+  m.init(); //call init in setup
+}
+
+void loop() {
+  // this should never run, Watchy deep sleeps after init();
+}
